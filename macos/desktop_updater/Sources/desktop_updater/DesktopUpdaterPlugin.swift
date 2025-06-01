71f92a5b33eb9b34eb6abdcfa6fadaa4d99fe3d3
TheFilyng <agustinbarruti@gmail.com>
2025-06-01 02:19:13 +0200

    Removes unused file copy function
    Removes the `copyAndReplaceFiles` function from the plugin.
    
    This function was not being used and its removal cleans up the codebase.

--- macos/desktop_updater/Sources/desktop_updater/DesktopUpdaterPlugin.swift
+++ macos/desktop_updater/Sources/desktop_updater/DesktopUpdaterPlugin.swift
@@ -67,47 +67,6 @@ public class DesktopUpdaterPlugin: NSObject, FlutterPlugin {
         NSApplication.shared.terminate(nil)
     }
 
-    
-    func copyAndReplaceFiles(from sourcePath: String, to destinationPath: String) throws {
-        let fileManager = FileManager.default
-        let enumerator = fileManager.enumerator(atPath: sourcePath)
-        
-        while let element = enumerator?.nextObject() as? String {
-            let sourceItemPath = (sourcePath as NSString).appendingPathComponent(element)
-            let destinationItemPath = (destinationPath as NSString).appendingPathComponent(element)
-            
-            var isDir: ObjCBool = false
-            if fileManager.fileExists(atPath: sourceItemPath, isDirectory: &isDir) {
-                if isDir.boolValue {
-                    // Ensure the directory exists at destination
-                    if !fileManager.fileExists(atPath: destinationItemPath) {
-                        try fileManager.createDirectory(atPath: destinationItemPath, withIntermediateDirectories: true, attributes: nil)
-                    }
-                } else {
-                    // Handle file or symbolic link
-                    let attributes = try fileManager.attributesOfItem(atPath: sourceItemPath)
-                    if attributes[.type] as? FileAttributeType == .typeSymbolicLink {
-                        // Handle symbolic link
-                        if fileManager.fileExists(atPath: destinationItemPath) {
-                            try fileManager.removeItem(atPath: destinationItemPath)
-                        }
-                        let target = try fileManager.destinationOfSymbolicLink(atPath: sourceItemPath)
-                        try fileManager.createSymbolicLink(atPath: destinationItemPath, withDestinationPath: target)
-                    } else {
-                        // Handle regular file
-                        if fileManager.fileExists(atPath: destinationItemPath) {
-                            // Replace existing file
-                            try fileManager.replaceItem(at: URL(fileURLWithPath: destinationItemPath), withItemAt: URL(fileURLWithPath: sourceItemPath), backupItemName: nil, options: [], resultingItemURL: nil)
-                        } else {
-                            // Copy new file
-                            try fileManager.copyItem(atPath: sourceItemPath, toPath: destinationItemPath)
-                        }
-                    }
-                }
-            }
-        }
-    }
-
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "desktop_updater", binaryMessenger: registrar.messenger)
         let instance = DesktopUpdaterPlugin()
